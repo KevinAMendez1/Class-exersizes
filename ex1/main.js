0b17de64dac2b08519d92531c57db7be00874f13
KevinAMendez1 <kevinamendez905@gmail.com>
2022-09-06 14:38:18 -0500

    finished exercise 1

--- ex1/main.js
+++ ex1/main.js
@@ -1,1 +1,7 @@
 const variable = 69;
+
+// EXERCISE 1
+function exercise(name = "Kevin Mendez", reasonForCoding = "further my education and abilities in todays world") {
+    console.log(`My name is ${name} and I code to ${reasonForCoding}`);
+}
+exercise();
